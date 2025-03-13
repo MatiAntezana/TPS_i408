e6710cf7fa269e8860879164aad1159aa5a23b96
Matías Antezana <mantezana@udesa.edu.ar>
2025-03-13 18:05:52 -0300

    cambios

--- tp1/Antezana_Pereyra.hs
+++
@@ -1,6 +0,0 @@
--- module Truck (Truck, newT, freeCellsT, loadT, unloadT, netT) where
---     data Truck = Tru[Stack] Route  -> Truck
---     freeCellsT :: Truck -> int
---     loadT :: Truck -> Palet -> Truck
---     unloadT :: Truck -> string -> Truck
---     netT :: Truck -> int
