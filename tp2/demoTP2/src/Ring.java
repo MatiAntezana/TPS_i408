d6d67ef766208fbe312762b40e834f7231d85a36
Matías Antezana <mantezana@udesa.edu.ar>
2025-03-27 23:55:51 -0300

    subo archivos

---
+++ tp2/demoTP2/src/Ring.java
@@ -0,0 +1,18 @@
+public class Ring {
+    public Ring next() {
+        if 
+        return null;
+    }
+
+    public Object current() {
+        return null;
+    }
+
+    public Ring add( Object cargo ) {
+        return null;
+    }
+
+    public Ring remove() {
+        return null;
+    }
+}
