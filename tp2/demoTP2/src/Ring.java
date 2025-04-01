01fd5e59fd479607770ae691cad81dbb5b58c96c
Matías Antezana <mantezana@udesa.edu.ar>
2025-03-31 22:27:21 -0300

    cambios

--- tp2/demoTP2/src/Ring.java
+++ tp2/demoTP2/src/Ring.java
@@ -1,18 +1,50 @@
+package anillo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ring {
+    private final List<Object> ring_data = new ArrayList<>();
+    private int pos_actual = -1;
+
     public Ring next() {
-        if 
-        return null;
+        if (ring_data.isEmpty()) {
+            throw new RuntimeException();
+        } else if (pos_actual - 1 < 0) {
+            pos_actual = ring_data.size() - 1;
+        } else {
+            pos_actual = (pos_actual - 1) % ring_data.size();
+        }
+        return this;
     }
 
     public Object current() {
-        return null;
+        if (ring_data.isEmpty()) {
+            throw new RuntimeException();
+        } else if (pos_actual == ring_data.size()) {
+            pos_actual = pos_actual % ring_data.size();
+        }
+        return ring_data.get(pos_actual);
+        // Puede ser que se pueda sacar el if
     }
 
-    public Ring add( Object cargo ) {
-        return null;
+    public Ring add(Object input) {
+        if (ring_data.isEmpty()) {
+            ring_data.add(input);
+            pos_actual += 1;
+        } else {
+            pos_actual += 1;
+            ring_data.add(pos_actual, input);
+        }
+        return this;
     }
 
     public Ring remove() {
-        return null;
+        ring_data.remove(pos_actual);
+        pos_actual -= 1;
+        if (pos_actual < 0) {
+            pos_actual = ring_data.size() - 1;
+        }
+        return this;
     }
 }
