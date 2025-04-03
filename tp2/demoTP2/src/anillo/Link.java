83b1eaac4f465256126f3fd94a2900dad918deb2
Julieta Guillermina Garcia Pereyra <jgarciapereyra@udesa.edu.ar>
2025-04-03 01:40:57 -0300

    Saco arrayList y creo Clase Link

---
+++ tp2/demoTP2/src/anillo/Link.java
@@ -0,0 +1,4 @@
+package anillo;
+
+public class Link {
+}
--- tp2/demoTP2/src/anillo/Ring.java
+++ tp2/demoTP2/src/anillo/Ring.java
@@ -3,31 +3,77 @@ package anillo;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Ring {
-    private final List<Object> ringData = new ArrayList<>();
-    private int currentPos = -1;
+    private Link current; // puntero al nodo actual dentro del anillo.
 
     public Ring next() {
-        currentPos = (currentPos - 1 + ringData.size()) % ringData.size();
+        current = current.getPrevious();
         return this;
     }
 
 
     public Object current() {
-        return ringData.get(currentPos);
+        emptyRingException();
+        return current.getData();
     }
 
 
-    public Ring add(Object input){
-        ringData.add(++currentPos % (ringData.size() + 1), input);
+    private void emptyRingException() {
+        if (current == null) throw new RuntimeException("Empty ring");
+    }
+
+
+    public Ring add( Object input ) {
+        Link newLink = new Link( input ); // se crea un nuevo eslabon link con el valor proporcionado.
+
+        if (current != null) {
+            current.insertAfter(newLink);
+        }
+        current = newLink;
         return this;
     }
 
 
     public Ring remove() {
-        ringData.remove(currentPos);
-        return ringData.isEmpty() ? this : next();
+        emptyRingException();
+        if (current.getPrevious() == current) {
+            current = null;
+        } else {
+            Link toRemove = current;
+            current = current.getPrevious();
+            toRemove.remove();
+        }
+        return this;
     }
-
-
 }
+
+
+//public class Ring {
+//    private final List<Object> ringData = new ArrayList<>();
+//    private int currentPos = -1;
+//
+//    public Ring next() {
+//        currentPos = (currentPos - 1 + ringData.size()) % ringData.size();
+//        return this;
+//    }
+//
+//
+//    public Object current() {
+//        return ringData.get(currentPos);
+//    }
+//
+//
+//    public Ring add(Object input){
+//        ringData.add(++currentPos % (ringData.size() + 1), input);
+//        return this;
+//    }
+//
+//
+//    public Ring remove() {
+//        ringData.remove(currentPos);
+//        return ringData.isEmpty() ? this : next();
+//    }
+//
+//
+//}
