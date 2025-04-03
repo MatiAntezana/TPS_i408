51fa0109ff77e4572e334d9c91cad495d01c259b
Julieta Guillermina Garcia Pereyra <jgarciapereyra@udesa.edu.ar>
2025-04-03 17:31:57 -0300

    Clase Link

--- tp2/demoTP2/src/anillo/Link.java
+++ tp2/demoTP2/src/anillo/Link.java
@@ -1,4 +1,37 @@
 package anillo;
 
 public class Link {
+    private final Object data; //almacena el valor del eslabón. Es de tipo object para que pueda contener cualquier tipo de dato.
+    private Link previous, next; //referencia al nodo anterior y siguiente en el anillo.
+
+
+    public Link( Object data) { // constructor
+        this.data = data;
+        this.previous = this.next = this;
+    }
+
+
+    public void insertAfter(Link newLink) {
+        newLink.next = this.next;
+        newLink.previous = this;
+        this.next.previous = newLink;
+        this.next = newLink;
+
+    }
+
+
+    public void remove() {
+        this.previous.next = this.next;
+        this.next.previous = this.previous;
+    }
+
+
+    public Object getData() {
+        return data;
+    }
+
+
+    public Link getPrevious() {
+        return previous;
+    }
 }
