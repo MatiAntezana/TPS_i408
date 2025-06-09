655df86f4d42da5a04dab9a2aaad0b91d8537acc
Matías Antezana <mantezana@udesa.edu.ar>
2025-06-09 09:42:00 -0300

    cambios

--- tp4/UnoBack/src/main/java/org/udesa/unoback/controller/UnoController.java
+++ tp4/UnoBack/src/main/java/org/udesa/unoback/controller/UnoController.java
@@ -1,4 +1,46 @@
 package org.udesa.unoback.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.udesa.unoback.model.Card;
+import org.udesa.unoback.model.JsonCard;
+import org.udesa.unoback.service.UnoService;
 
+import java.rmi.server.UID;
+import java.util.UUID;
+import java.util.List;
+
+@RestController
 public class UnoController {
+    @Autowired
+    private final UnoService unoService;
+
+    public UnoController(UnoService unoService) {
+        this.unoService = unoService;
+    }
+
+    @PostMapping("newmatch")
+    public ResponseEntity newMatch(@RequestParam List<String> players) {
+        // Quiero pasarle un newMatch a un servicio
+        return ResponseEntity.ok(unoService.newMatch(players));
+    }
+    @PostMapping("play/{matchId}/{player}") public ResponseEntity play(@PathVariable UUID matchId, @PathVariable String player, @RequestBody JsonCard card ) {
+        Card playableCard = card.asCard();
+        unoService.playCard(matchId, player, playableCard);
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("draw/{matchId}/{player}") public ResponseEntity drawCard( @PathVariable UUID matchId, @PathVariable String player ) {
+        unoService.drawCard(matchId, player);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("activecard/{matchId}") public ResponseEntity activeCard( @PathVariable UUID matchId ) {
+        return ResponseEntity.ok(unoService.getActiveCard(matchId).asJson());
+    }
+
+    @GetMapping("playerhand/{matchId}")
+    public ResponseEntity playerHand(@PathVariable UUID matchId) {
+        return ResponseEntity.ok(unoService.playerHand(matchId).stream().map(each -> each.asJson()));
+    }
 }
--- tp4/UnoBack/src/main/java/org/udesa/unoback/service/Dealer.java
+++ tp4/UnoBack/src/main/java/org/udesa/unoback/service/Dealer.java
@@ -1,4 +1,38 @@
 package org.udesa.unoback.service;
+import org.springframework.stereotype.Component;
+import org.udesa.unoback.model.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
 public class Dealer {
+
+    public List<Card> fullDeck() {
+        ArrayList<Card> deck = new ArrayList<>();
+        deck.addAll(CardOn("Red"));
+        deck.addAll(CardOn("Blue"));
+        deck.addAll(CardOn("Green"));
+        deck.addAll(CardOn("Yellow"));
+        return deck;
+    }
+
+    public List<Card> CardOn(String color) {
+        return List.of(new SkipCard(color),
+                new ReverseCard(color), new Draw2Card(color),
+                new NumberCard(color, 0),
+                new NumberCard(color,1),
+                new NumberCard(color, 2),
+                new NumberCard(color, 3),
+                new NumberCard(color, 4),
+                new NumberCard(color, 5),
+                new NumberCard(color, 6),
+                new NumberCard(color, 7),
+                new NumberCard(color, 8),
+                new NumberCard(color, 9),
+                new WildCard()
+                );
+    }
+
 }
